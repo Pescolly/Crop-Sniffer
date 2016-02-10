f5acbc6dab8b0414c70ddbe3350a1cec6e509c78
Armen Karamian <akaramian@tv-met-mac-3.modern.mvfinc.com>
2016-02-10 10:10:03 -0800

    Initial Commit

---
+++ CropSniffer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CropSniffer
+//
+//  Created by Armen Karamian on 2/10/16.
+//  Copyright © 2016 Armen Karamian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
